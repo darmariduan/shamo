e2f7da7c6e4d23896d29e3d12fe45433938b981a
darmariduan <dharmarhiduwhan@gmail.com>
2024-01-20 00:36:05 +0700

    Add Kategori

---
+++ resources/views/pages/dashboard/category/create.blade.php
@@ -0,0 +1,51 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Category &raquo; Create
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div>
+                @if ($errors->any())
+                    <div class="mb-5" role="alert">
+                        <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+                            There's something wrong!
+                        </div>
+                        <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+                            <p>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                            </p>
+                        </div>
+                    </div>
+                @endif
+                <form action="{{ route('dashboard.category.store') }}" method="post" class="w-full"
+                    enctype="multipart/form-data">
+                    @csrf
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                                for="grid-last-name">
+                                Name
+                            </label>
+                            <input value="{{ old('name') }}" name="name"
+                                class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
+                                id="grid-last-name" type="text" placeholder="Category Name">
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <button type="submit"
+                                class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                                Save Category
+                            </button>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
